9b61a335c3bb24021428f9c40c7e09dc16a745ee
Lauren <lburke522@gmail.com>
2022-11-15 17:48:56 -0500

    got basic functionality down, need to select words and end game

---
+++ client/src/App.jsx
@@ -0,0 +1,79 @@
+import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { createContext, useState } from "react";
+import { boardDefault, generateWordSet } from "./Words";
+import { useEffect } from "react";
+
+export const AppContext = createContext();
+
+function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({
+    attempt: 0,
+    letterPosition: 0,
+  });
+  const [wordSet, setWordSet] = useState(new Set());
+
+  const correctWord = "RIGHT";
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+    });
+  });
+
+  const onSelectLetter = (keyValue) => {
+    if (currAttempt.letterPosition > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPosition] = keyValue;
+    setBoard(newBoard);
+    setCurrAttempt({
+      ...currAttempt,
+      letterPosition: currAttempt.letterPosition + 1,
+    });
+  };
+
+  const onEnter = () => {
+    if (currAttempt.letterPosition !== 5) return;
+    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPosition: 0 });
+  };
+
+  const onDelete = () => {
+    if (currAttempt.letterPosition === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPosition - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({
+      ...currAttempt,
+      letterPosition: currAttempt.letterPosition - 1,
+    });
+  };
+
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrAttempt,
+          onSelectLetter,
+          onEnter,
+          onDelete,
+          correctWord,
+        }}
+      >
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Words.jsx
@@ -0,0 +1,21 @@
+import wordBank from './word-bank.txt';
+
+export const boardDefault = [
+    ["", "", "", "", "", ],
+    ["", "", "", "", "", ],
+    ["", "", "", "", "", ],
+    ["", "", "", "", "", ],
+    ["", "", "", "", "", ],
+    ["", "", "", "", "", ]
+];
+
+export const generateWordSet = async () => {
+    let wordSet;
+    await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+    const wordArr = result.split("\n");
+    wordSet = new Set(wordArr);
+ });
+ return { wordSet }
+}

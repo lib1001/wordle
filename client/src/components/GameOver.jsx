1ffa5ad59362cc5e2acac94c0300da4f0f41ccfa
Lauren <lburke522@gmail.com>
2022-11-25 13:07:17 -0500

    legacy peer deps

--- client/src/components/GameOver.jsx
+++ client/src/components/GameOver.jsx
@@ -7,7 +7,7 @@ function GameOver() {
     <div className='gameOver'>
         <h3>{gameOver.guessedWord ? "Woohoo! You nailed it!" : "Better Luck Next Time.."} </h3>
         <h1>Correct Word: {correctWord}</h1>
-        {gameOver.guessedWord && (<h3>You guessed it {currAttempt.attempt} attempts</h3>)}
+        {gameOver.guessedWord && (<h3>You guessed in {currAttempt.attempt} attempts</h3>)}
     </div>
   )
 }
